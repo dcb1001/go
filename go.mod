b5cf933ece0caaa58baf3d358709fd95247dbd33
Piyush Chauhan <cpiyush@tower-research.com>
2024-02-06 16:29:52 +0530

    inital commit
    Change-Id: I43e19f8480d5671be189f5ce2cfb01613c13327f

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dcb1001/go
+
+go 1.18
---
+++ hello.go
@@ -0,0 +1,7 @@
+package testpkg
+
+import "fmt"
+
+func Helper_testpkg () {
+	fmt.Println ("this is helper function of testpkg from github")
+}
